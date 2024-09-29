f8e300e227aa66b781322de0d1ce93e85456a789
Louis Radek <radek.louis@gmail.com>
2024-09-29 18:47:52 +0200

    feat(input): add create_highlight_mesh

--- src/ui-game/input.rs
+++ src/ui-game/input.rs
@@ -1,9 +1,10 @@
-use ggez::{Context, graphics};
-use ggez::glam::Vec2;
-use ggez::graphics::{Color, DrawMode};
-use muehle::action::{self, list_actions, Action};
-use muehle::game_state::{self, GameState, Phase, Token};
+use ggez::Context;
+use ggez::graphics::Canvas;
+use muehle::action::{list_actions, Action};
+use muehle::game_state::{GameState, Phase, Token};
 use muehle::position::get_token_at;
+
+use crate::ui::{get_token_draw_params, GameResources};
 
 pub struct InputHandler {
     game_state: GameState,
@@ -12,10 +13,9 @@ pub struct InputHandler {
     state: InputHandlerState,
     selected_pos: Option<usize>,
     selected_action: Option<Action>,
-    selected_take: Option<usize>,
 }
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 enum InputHandlerState {
     PlaceDest,
     Source,
@@ -45,8 +45,7 @@ impl InputHandler {
                 Phase::Move => { InputHandlerState::Source }
             }),
             selected_pos: None,
-            selected_action: None,
-            selected_take: None
+            selected_action: None
         }
     }
 
@@ -92,7 +91,9 @@ impl InputHandler {
                     self.selected_action = Some(action);
                 }
                 InputHandlerState::Take => {
-                    self.selected_action.unwrap().set_beatable_position(Some(position));
+                    if let Some(action) = self.selected_action.as_mut() {
+                        action.beatable_position = Some(position);
+                    }
                     self.state = InputHandlerState::Done;
                 }
                 InputHandlerState::Done => {}
@@ -121,36 +122,31 @@ impl InputHandler {
         }
     }
 
-    // pub fn create_highlight_mesh(&self, ctx: &mut Context, hover: Option<Pos>) -> graphics::Mesh {
-    //     let mb = &mut graphics::MeshBuilder::new();
-
-    //     if let Some(action) = self.selected_action {
-    //         match action {
-    //             Action::Place(dest) => {
-    //                 mb.circle(DrawMode::fill(), Vec2::from(screen_pos(dest)), 10.0, 0.1, Color::from_rgb(229, 124, 35)).unwrap();
-    //             }
-    //             Action::Move(src, dest) => {
-    //                 mb.circle(DrawMode::stroke(2.5), Vec2::from(screen_pos(src)), 22.5, 0.1, Color::from_rgb(229, 124, 35)).unwrap();
-    //                 mb.circle(DrawMode::fill(), Vec2::from(screen_pos(dest)), 10.0, 0.1, Color::from_rgb(229, 124, 35)).unwrap();
-    //             }
-    //         }
-    //     } else if let Some(src) = self.selected_pos {
-    //         mb.circle(DrawMode::stroke(2.5), Vec2::from(screen_pos(src)), 22.5, 0.1, Color::from_rgb(229, 124, 35)).unwrap();
-    //     }
+    pub fn create_highlight_mesh(&self, ctx: &mut Context, canvas: &mut Canvas, resources: GameResources) {
+        if let Some(src) = self.selected_pos {
+            let outline_draw_params = get_token_draw_params(ctx, src, resources.clone());
+            canvas.draw(&resources.token_green_outline, outline_draw_params)
+        }
         
-    //     for pos in POSITIONS {
-    //         if self.can_click(pos) {
-    //             mb.circle(DrawMode::fill(), Vec2::from(screen_pos(pos)), 8.0, 0.1, Color::from_rgba(232, 170, 66, 165)).unwrap();
-    //         }
-    //     }
-    //     if let Some(pos) = hover {
-    //         if self.can_click(pos) {
-    //             mb.circle(DrawMode::stroke(2.5), Vec2::from(screen_pos(pos)), 22.5, 0.1, Color::from_rgba(232, 170, 66, 165)).unwrap();
-    //         }
-    //     }
-
-    //     graphics::Mesh::from_data(ctx, mb.build())
-    // }
+        for position in 0..24 {
+            if self.can_click(position) {
+                if self.state == InputHandlerState::PlaceDest {
+                    let outline_draw_params = get_token_draw_params(ctx, position, resources.clone());
+                    canvas.draw(&resources.empty_token_outline, outline_draw_params)
+                }
+                
+                if self.state == InputHandlerState::Source {
+                    let outline_draw_params = get_token_draw_params(ctx, position, resources.clone());
+                    canvas.draw(&resources.token_green_outline, outline_draw_params)
+                }
+                
+                if self.state == InputHandlerState::Take {
+                    let outline_draw_params = get_token_draw_params(ctx, position, resources.clone());
+                    canvas.draw(&resources.token_red_outline, outline_draw_params)
+                }
+            }
+        }
+    }
     
     pub fn hint(&self) -> &'static str {
         match self.state {
